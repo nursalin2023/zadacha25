34ad3e25517cba9f886b9454634bd32f5f57d93e
nurdolotmamasabirov <nurdolotmamasabirov225@gmail.com>
2023-03-18 12:51:42 +0600

    Nurdolot commit

---
+++ src/Car.java
@@ -0,0 +1,30 @@
+public class Car {
+    String marka;
+    String model;
+    String color;
+
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
